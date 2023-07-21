ec8a085d569a5bab23c9ded0c0f622a20634d90e
Leo007ua <leo.oliinyk@ukr.net>
2023-07-21 07:17:41 +0300

    add request and page home

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+// import { lazy } from "react";
+// import { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <header>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/movies">Movies</Link>
+      </nav>
+      </header>
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="*" element={<NotFound />} /> */}
+      </Routes>
     </div>
   );
 };
+
+
+
+// const MarkdownPreview = lazy(() => import('./MarkdownPreview.js'));
+{/* <Suspense fallback={<Loading />}> */}
