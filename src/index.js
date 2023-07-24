9d1bf05501b76c320d3f52ce415535c5cd46164a
Leo007ua <leo.oliinyk@ukr.net>
2023-07-24 16:16:20 +0300

    add change app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'components/APP';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import App from 'components/App';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/goit-react-hw-05-movies'>
+    <BrowserRouter>
     <App />
     </BrowserRouter>
   </React.StrictMode>
